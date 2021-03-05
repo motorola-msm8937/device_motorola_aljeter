4a734593c010d7c467902e04bb0d2fb26a2abeb4
menorziin <williamsilvapppl@gmail.com>
2021-03-04 22:58:35 -0300

    jeter: sensors: fix for aljeter

--- device.mk
+++ device.mk
@@ -68,7 +68,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev_aljeter.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev_aljeter.conf
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
